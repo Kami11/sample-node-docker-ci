0d0324c9039d573fadffb5c40131a9eee4ad84ed
kami11 <nadyaenerg141@mail.ru>
2017-04-06 06:59:48 +0000

    fix3

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 docker push kami11/sample-node
 
 ssh kamikadzeim11@35.187.62.117 << EOF
-docker push kami11/sample-node:latest
+
+#docker push kami11/sample-node:latest
+docker pull kami11/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi kami11/sample-node:current || true
 docker tag kami11/sample-node:latest kami11/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kami11/sample-node:current
+docker run -d --restart always --name web -p 80:80 kami11/sample-node:current
 EOF
